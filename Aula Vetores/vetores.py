2a4acbfa538522d461ca790e95094dc163bdd4e0
Luiza Assis <luizaassis57@gmail.com>
2026-08-18 16:27:36 -0300

    Add files via upload

---
+++ Aula Vetores/vetores.py
@@ -0,0 +1,23 @@
+numeros = [33,11,22,54,51,52,17]
+nomes = ["Julia", "Maria", "Luiza", "Enzo"]
+
+#append - Insere um novo valor no vetor, sempre na ultima posição
+# numeros.append(67)
+
+#insert - Insere um novo valor na posição desejada
+# numeros.insert(3,50)
+
+#pop - Deleta um valor pela sua posição
+# numeros.pop(0)
+
+#remove - Deleta um valor pelo seu conteudo
+numeros.remove(54)
+nomes.remove("Enzo")
+print(nomes)
+print(numeros)
+#sort
+#reverse
+#len
+#Count
+#sum()
+#max - min
