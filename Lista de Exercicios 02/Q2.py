aeebdce2b84c3f2798bcd5cc05891b07a802e7ce
Luiza Assis <luizaassis57@gmail.com>
2026-08-25 16:21:53 -0300

    Segunda questão

--- Lista de Exercicios 02/Q2.py
+++ Lista de Exercicios 02/Q2.py
@@ -1,6 +1,15 @@
-import os
-os.system("cls")
+import random, os
 
 # 2) Crie um programa em Python que lê uma lista de 10 nomes e sorteia um
 # nome entre eles.
 
+nomes = []
+
+for i in range (10):
+    nome = input(f"Digite o {i+1}º nome: ")
+    nomes.append(nome)
+    os.system("cls" or "clear")
+    
+sortudo = random.choice(nomes)
+
+print(f"Você {sortudo} foi o sorteado!")
